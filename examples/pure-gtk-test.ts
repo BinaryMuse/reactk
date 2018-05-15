19d371d970b37265764f70344ee94130a9428336
Michelle Tilley <michelle@michelletilley.net>
2018-05-15 13:08:31 -0700

    Add React, Typescript

---
+++ examples/pure-gtk-test.ts
@@ -0,0 +1,23 @@
+import { Gtk as gtk } from "node-gir";
+
+const win = new gtk.Window({
+  type: gtk.WindowType.TOPLEVEL,
+  title: "Node.js Test GTK Window"
+});
+win.setBorderWidth(10);
+
+const button = new gtk.Button();
+button.setLabel("Hello, world!");
+win.add(button);
+
+win.connect("destroy", () => {
+  gtk.mainQuit();
+});
+
+let clickCount = 0;
+button.connect("clicked", () => {
+  button.setLabel(`Clicked ${++clickCount} times`);
+});
+
+win.showAll();
+gtk.main();
